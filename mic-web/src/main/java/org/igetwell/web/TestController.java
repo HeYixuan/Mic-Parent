283d9a1eb322afa82515bb0e86079a94f581e1c4
HeYixuan <15517551511@126.com>
2018-06-08 19:27:25 +0800

    tijiao

--- mic-web/src/main/java/org/igetwell/web/TestController.java
+++ mic-web/src/main/java/org/igetwell/web/TestController.java
@@ -1,10 +1,20 @@
 package org.igetwell.web;
 
+import com.alibaba.fastjson.JSONObject;
+import org.igetwell.common.utils.WeChatUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class TestController {
 
+    @GetMapping("/test")
+    @ResponseBody
+    public void test(){
+        JSONObject json = WeChatUtils.getAuthorizationCode();
+        System.err.println(json);
+    }
 }
