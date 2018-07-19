68318736fdcc2e3a2ae85c6bd534788cc25f512e
HeYixuan <15517551511@126.com>
2018-07-19 16:41:43 +0800

    微信支付

--- mic-web/src/main/java/org/igetwell/web/pay/WxPayController.java
+++ mic-web/src/main/java/org/igetwell/web/pay/WxPayController.java
@@ -2,10 +2,10 @@ package org.igetwell.web.pay;
 
 import org.igetwell.common.enums.JsApiType;
 import org.igetwell.local.HttpKit;
-import org.igetwell.local.IpKit;
 import org.igetwell.local.LocalPay;
 import org.igetwell.web.BaseController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +25,7 @@ public class WxPayController extends BaseController {
      * 扫码支付
      * @return
      */
-    @RequestMapping("/scanPay")
+    @PostMapping("/scanPay")
     @ResponseBody
     public String scanPay() {
         String codeUrl = localPay.scanOrder(request.get(), "官网费用","GW201807162055","1");
@@ -38,7 +38,7 @@ public class WxPayController extends BaseController {
      * 微信H5、APP内调起支付
      * @return
      */
-    @RequestMapping("/preOrder")
+    @PostMapping("/preOrder")
     @ResponseBody
     public Map<String, String> preOrder() {
         return localPay.preOrder(request.get(), null, JsApiType.APP,"官网费用","GW201807162055","1");
@@ -48,8 +48,7 @@ public class WxPayController extends BaseController {
     /**
      * 微信支付返回通知
      */
-    @RequestMapping(value = "payNotify",produces = {"application/xml"})
-    @ResponseBody
+    @PostMapping(value = "payNotify", produces = {"application/xml"})
     public void payNotify(){
         try {
             String xmlStr = HttpKit.readData(request.get());
