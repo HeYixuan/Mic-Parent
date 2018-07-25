b24f7897f746aead13a6ce0d3cb3bc0d649260fd
HeYixuan <15517551511@126.com>
2018-07-25 18:15:23 +0800

    HttpClient带请求头

--- common-utils/src/main/java/org/igetwell/common/utils/HttpClientUtil.java
+++ common-utils/src/main/java/org/igetwell/common/utils/HttpClientUtil.java
@@ -1,5 +1,6 @@
 package org.igetwell.common.utils;
   
+import com.alibaba.fastjson.JSON;
 import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.config.RequestConfig;
@@ -19,6 +20,8 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
@@ -30,10 +33,10 @@ import java.util.Set;
 
 /**
  * HttpClient工具类
- * @author wangfucai
  */
+public class HttpClientUtil {
+    private static Logger logger = LoggerFactory.getLogger(HttpClientUtil.class);
 
-public class HttpClientUtil {  
     private RequestConfig requestConfig = RequestConfig.custom()  
         .setSocketTimeout(15000)  
         .setConnectTimeout(15000)  
@@ -97,59 +100,81 @@ public class HttpClientUtil {
         return sendHttpPost(httpPost,charset);    
     }
 
+    /**
+     * 发送 post请求
+     * @param httpUrl 地址
+     * @param headers 请求头
+     * @param params 参数
+     * @param charset
+     * @return
+     */
+    public String sendHttpPost(String httpUrl, Map<String, String> headers, Map<String, Object> params, String charset){
+        HttpPost httpPost = new HttpPost(httpUrl);// 创建httpPost
+        for(Map.Entry<String, String> entry : headers.entrySet()){
+            httpPost.setHeader(entry.getKey(), entry.getValue());
+        }
+        if (params.isEmpty()){
+            logger.error("请求参数不能为空!");
+            return null;
+        }
+        try {
+            String json = JSON.toJSONString(params);
+            StringEntity entity = new StringEntity(json, "UTF-8");
+            entity.setContentEncoding("UTF-8");
+            entity.setContentType("application/json");
+            httpPost.setEntity(entity);
+
+        } catch (Exception e) {
+            logger.error("sendHttpPost请求失败：", e);
+        }
+        return sendHttpPost(httpPost, charset);
+    }
 
     /**
-     * 上传文件
-     *
-     * @param url
-     *            服务器地址
-     * @param localFilePath
-     *            本地文件路径
-     * @param serverFieldName
-     * @param params
-     * @return
-     * @throws Exception
+     * 发送 post请求（带文件）
+     * @param httpUrl 地址
+     * @param maps 参数
+     * @param fileMap 附件
      */
-    public String multipartFile(String url, String localFilePath, String serverFieldName, Map<String, String> params){
-        String responseContent = null;
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        CloseableHttpClient httpClient = null;
-        CloseableHttpResponse response = null;
-        HttpEntity entity = null;
-        try {
-            HttpPost httppost = new HttpPost(url);
-            FileBody binFileBody = new FileBody(new File(localFilePath));
-
-            MultipartEntityBuilder multipartEntityBuilder = MultipartEntityBuilder
-                    .create();
+    public String multipartFile(String httpUrl, Map<String, String> maps, Map<String, File> fileMap, String charset) {
+        HttpPost httpPost = new HttpPost(httpUrl);// 创建httpPost
+        MultipartEntityBuilder multipartEntityBuilder = MultipartEntityBuilder.create();
+        for(Map.Entry<String, File> entry : fileMap.entrySet()){
+            FileBody fileBody = new FileBody(entry.getValue());
             // add the file params
-            multipartEntityBuilder.addPart(serverFieldName, binFileBody);
-            // 设置上传的其他参数
-            setUploadParams(multipartEntityBuilder, params);
-
-            HttpEntity reqEntity = multipartEntityBuilder.build();
-            httppost.setEntity(reqEntity);
-
-            response = httpclient.execute(httppost);
-
-            entity = response.getEntity();
-            responseContent = EntityUtils.toString(entity, "UTF-8");
-        }catch (Exception e){
-
-        } finally {
-            try {
-                // 关闭连接,释放资源
-                if (response != null) {
-                    response.close();
-                }
-                if (httpClient != null) {
-                    httpClient.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            multipartEntityBuilder.addPart(entry.getKey(), fileBody);
         }
-        return responseContent;
+        // 设置上传的其他参数
+        setUploadParams(multipartEntityBuilder, maps);
+        HttpEntity entity = multipartEntityBuilder.build();
+        httpPost.setEntity(entity);
+        return sendHttpPost(httpPost,charset);
+    }
+
+    /**
+     * 发送 post请求（带文件）
+     * @param httpUrl 地址
+     * @param maps 参数
+     * @param fileMap 附件
+     */
+    public String multipartFile(String httpUrl, Map<String, String> headers, Map<String, String> maps, Map<String, File> fileMap, String charset) {
+        HttpPost httpPost = new HttpPost(httpUrl);// 创建httpPost
+        for(Map.Entry<String, String> entry : headers.entrySet()){
+            httpPost.setHeader(entry.getKey(), entry.getValue());
+        }
+        MultipartEntityBuilder multipartEntityBuilder = MultipartEntityBuilder.create();
+        for(Map.Entry<String, File> entry : fileMap.entrySet()){
+            FileBody fileBody = new FileBody(entry.getValue());
+            // add the file params
+            multipartEntityBuilder.addPart(entry.getKey(), fileBody);
+        }
+
+        // 设置上传的其他参数
+        setUploadParams(multipartEntityBuilder, maps);
+        HttpEntity entity = multipartEntityBuilder.build();
+        httpPost.setEntity(entity);
+
+        return sendHttpPost(httpPost,charset);
     }
 
     /**
